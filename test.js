2a3da8e0b7dbc63444b9ecc6488b7025dffaaa6a
Oscar Wu <oscar821210@gmail.com>
2018-10-26 17:11:50 +1100

    update with jest

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ describe('String#split', function(){
     assert(Array.isArray('a,b,c'.split(',')));
   });
 });
+
+
+describe('add operation', function(){
+  it('should return an array', function(){
+    expect(true).toEqual(true);
+  });
+});
